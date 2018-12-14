003a41f597e50565db8b62626e0bc2da30da41e5
Yago Pessoa <yagorp15@gmail.com>
2018-12-14 20:03:29 -0200

    Changed background color of homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -114,7 +114,8 @@
   .bottom-session{
     /* min-height: 100vh; */
     width: 100%;
-    background-color: #F1CD36;
+    background-color: rgba(241,205,54, 0.3);
+    /* background-color: rgba(53,74,95, 0.3); */
     padding-top: 60px;
   }
   p{
