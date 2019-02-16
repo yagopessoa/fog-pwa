fe21e598800d915e372a875fe1325d74a3347873
Yago Pessoa <yagorp15@gmail.com>
2019-02-16 12:23:06 -0200

    Events page created, missing GameLink logo

--- src/pages/EventsPage.vue
+++ src/pages/EventsPage.vue
@@ -1,3 +1,205 @@
 <template>
-  <h1>Eventos</h1>
+  <div>
+    <v-parallax class="parallax" src="https://i.ibb.co/pQBj77p/bg2.jpg">
+      <div class="upper">
+        <v-container class="upper-container" grid-list-md>
+          <v-layout row wrap>
+            <v-flex xs12 md8 offset-md2>
+              <div class="title-n-subtitle text-xs-justify ma-2 black--text">
+
+                <p class="headline hidden-xs-only mt-4">
+                  <u><b>USP Game Link</b></u> é um evento anual organizado pelo <b>FoG</b>, cujo objetivo é unir e fortalecer a <b>comunidade de 
+                  desenvolvedores de jogos da região</b>, permitindo um amadurecimento e crescimento da indústria brasileira de jogos, 
+                  de modo a atingirmos uma relevância ainda maior no cenário mundial de jogos.
+                </p>
+
+                <p class="headline hidden-sm-and-up">
+                  USP Game Link
+                </p>
+                <p class="subheading hidden-sm-and-up">
+                  é um evento anual organizado pelo <b>FoG</b>, cujo objetivo é unir e fortalecer a <b>comunidade de 
+                  desenvolvedores de jogos da região</b>, permitindo um amadurecimento e crescimento da indústria brasileira de jogos, 
+                  de modo a atingirmos uma relevância ainda maior no cenário mundial de jogos.
+                </p>
+
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </div>
+    </v-parallax>
+
+    <div class="bottom">
+
+      <v-container class="upper-container text-xs-justify" grid-list-md>
+        <v-layout row wrap>
+          <v-flex class="bottom-container" xs12 md8 offset-md2>
+
+            <div class="hidden-xs-only">
+              <p class="headline text-xs-center mt-4 pa-2 px-4 pt-4">
+                Um evento, dois dias
+              </p>
+              
+              <p class="subheading mt-2">
+                <u><b>O primeiro dia</b></u> do evento conta com a clássica <b>Mostra de Jogos do ICMC</b>, na qual diversos desenvolvedores de jogos brasileiros trazem seus trabalhos para que o público teste e se divirta, podendo também tirar dúvidas e dar sugestões para os próprios criadores do jogo!
+              </p>
+              <p class="subheading mt-1 pb-3">
+                Também contamos com a presença dos projetos da <b><router-link to="/disciplina">Disciplina de Desenvolvimento de Jogos</router-link></b>, oferecida pelo ICMC em colaboração com o FoG.
+              </p>
+
+              <p class="subheading mt-2">
+                <u><b>O segundo dia</b></u> é cheio de <b>palestras</b> e conversas sobre o desenvolvimento de jogos. Grandes nomes do mercado de jogos brasileiro falam sobre diversos assuntos, contam suas experiências de mercado e muito mais!
+              </p>
+            </div>
+
+            <div class="hidden-sm-and-up text-xs-justify ma-2 pa-3 black--text">
+              <p class="headline text-xs-center mt-2 pa-2">
+                Um evento, dois dias
+              </p>
+              
+              <p class="subheading mt-2">
+                <u><b>O primeiro dia</b></u> do evento conta com a clássica <b>Mostra de Jogos do ICMC</b>, na qual diversos desenvolvedores de jogos brasileiros trazem seus trabalhos para que o público teste e se divirta, podendo também tirar dúvidas e dar sugestões para os próprios criadores do jogo!
+              </p>
+              <p class="subheading mt-1 pb-4">
+                Também contamos com a presença dos projetos da <b><router-link to="/disciplina">Disciplina de Desenvolvimento de Jogos</router-link></b>, oferecida pelo ICMC em colaboração com o FoG.
+              </p>
+
+              <p class="subheading mt-2">
+                <u><b>O segundo dia</b></u> é cheio de <b>palestras</b> e conversas sobre o desenvolvimento de jogos. Grandes nomes do mercado de jogos brasileiro falam sobre diversos assuntos, contam suas experiências de mercado e muito mais!
+              </p>
+            </div>
+
+            <p class="headline text-xs-center mt-4 pa-2 py-4 pt-2">
+              Fique ligado na próxima edição!
+            </p>
+
+            <p class="subheading text-xs-center">
+              <v-icon left class="black--text">today</v-icon>
+              9 e 10 de Novembro de 2019
+            </p>
+            <p class="subheading text-xs-center mb-4 pb-4">
+              <v-icon left class="black--text">place</v-icon>
+              ICMC - USP - São Carlos
+            </p>
+
+            <v-btn 
+              href="https://www.facebook.com/events/366436447425350/"
+              target="_blank" 
+              color="accent"
+              contained
+              large
+            >
+              <div class="indigo--text text--darken-4">Página do evento</div>
+              <v-icon right class="indigo--text text--darken-4">open_in_new</v-icon>
+            </v-btn>
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <p class="display-1 text-xs-center mt-2 pt-4">Edição 2018:</p>
+      <v-layout row wrap>
+        <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+          <v-carousel class="mb-4">
+            <v-carousel-item
+              v-for="(item,i) in items"
+              :key="i"
+              :src="item.src"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+
+      <p class="subheading text-xs-center mt-4 mb-4 pa-4">
+        Ficou com alguma dúvida? Contate-nos pela página do <b><a href="https://www.facebook.com/fogicmc" target="_blank">Facebook</a></b>!
+      </p>
+    </div>
+
+  </div>
 </template>
+
+<script>
+  export default {
+    name: 'EventsPage',
+    data () {
+      return {
+        items: [
+          {
+            src: '/static/img/gamelink/1.jpg'
+          },
+          {
+            src: '/static/img/gamelink/2.jpg'
+          },
+          {
+            src: '/static/img/gamelink/3.jpg'
+          },
+          {
+            src: '/static/img/gamelink/4.jpg'
+          },
+          {
+            src: '/static/img/gamelink/5.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .upper{
+    min-height: calc(100vh - 60px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .upper-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .bottom{
+    background-color: rgba(53, 74, 95, 0.2);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+  }
+  .bottom-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .title-n-subtitle{
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    align-items: center;
+  }
+  .brasao{
+    padding: 16px;
+    max-height: 80vh;
+  }
+  .brasao-small{
+    max-width: 75%;
+  }
+  .parallax{
+    min-height: calc(100vh - 60px);
+    object-fit: cover;
+  }
+
+  @media screen and (max-width: 959px) {
+    .title-n-subtitle{
+      padding: 16px;
+    }
+    .parallax{
+      min-height: calc(100vh - 48px);
+    }
+  }
+  @media screen and (max-height: 639px) {
+    .parallax{
+      min-height: calc(100vh);
+    }
+  }
+</style>
