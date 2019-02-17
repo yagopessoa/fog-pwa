b7846b589e0a024696524cadd35c3d9ffb96eaf1
Yago Pessoa <yagorp15@gmail.com>
2019-02-17 13:17:29 -0300

    Added list of members

---
+++ src/members.js
@@ -0,0 +1,252 @@
+const members = [
+  {
+    name: 'Abner Eduardo Silveira Santos',
+    img: 'abner.jpg'
+  },
+  {
+    name: 'Amira Gayatri El Assad Hempel',
+    img: 'amira.jpg'
+  },
+  {
+    name: 'Augusto Paulo Silva',
+    img: 'augusto.jpg'
+  },
+  {
+    name: 'Bruno Baldissera Carlotto',
+    img: null
+  },
+  {
+    name: 'Bruno Gazoni',
+    img: null
+  },
+  {
+    name: 'Daniel Fróes',
+    img: 'froes.jpg'
+  },
+  {
+    name: 'Daniel Sá Barretto Prado Garcia',
+    img: null
+  },
+  {
+    name: 'David Souza Rodrigues',
+    img: 'david.jpg'
+  },
+  {
+    name: 'Edson Yudi Toma',
+    img: 'edson.jpg'
+  },
+  {
+    name: 'Eike João Sanches Sato',
+    img: null
+  },
+  {
+    name: 'Eleazar Fernando Braga',
+    img: 'eleazar.jpg'
+  },
+  {
+    name: 'Elisa Saltori Trujillo',
+    img: 'elisa.jpg'
+  },
+  {
+    name: 'Enzo Paulo Sanches Sato',
+    img: null
+  },
+  {
+    name: 'Fabiana Dalacqua Mendes',
+    img: 'fabiana.jpg'
+  },
+  {
+    name: 'Fabrício Guedes Faria',
+    img: 'bibicio.jpg'
+  },
+  {
+    name: 'Felipe Contini Torres',
+    img: null
+  },
+  {
+    name: 'Fernando Gorgulho Fayet',
+    img: 'fer.jpg'
+  },
+  {
+    name: 'Gabriel Arantes de Lima e Silva',
+    img: 'lombardi.jpg'
+  },
+  {
+    name: 'Gabriel Mattheus Bezerra Alves de Carvalho',
+    img: 'gabriel.jpg'
+  },
+  {
+    name: 'Gabriel Romualdo Silveira Pupo',
+    img: 'pupo.jpg'
+  },
+  {
+    name: 'Gabriel Simmel Nascimento',
+    img: 'cartaz.jpg'
+  },
+  {
+    name: 'Giovanni Paolo Meloni',
+    img: 'giovanni.jpg'
+  },
+  {
+    name: 'Giuliano Lourençon',
+    img: 'psy.jpg'
+  },
+  {
+    name: 'Guilherme López Barros',
+    img: null
+  },
+  {
+    name: 'Gustavo de Moura Souza',
+    img: null
+  },
+  {
+    name: 'Gustavo Sasaki Roncaglia',
+    img: 'sasaki.jpg'
+  },
+  {
+    name: 'Gyovana Mayara Moriyama',
+    img: 'gyo.jpg'
+  },
+  {
+    name: 'Henrique Matarazo Camillo',
+    img: 'nintendo.jpg'
+  },
+  {
+    name: 'Jean Sidharta Rocha Guzman',
+    img: null
+  },
+  {
+    name: 'Jorge Luiz Bulhões Tosta',
+    img: 'jorge.jpg'
+  },
+  {
+    name: 'Leonardo Chieppe',
+    img: null
+  },
+  {
+    name: 'Leonardo Maronezi Pizani',
+    img: 'maronezi.jpg'
+  },
+  {
+    name: 'Leonardo Tórtoro Pereira',
+    img: 'leo.jpg'
+  },
+  {
+    name: 'Livia Scienza',
+    img: null
+  },
+  {
+    name: 'Lucas Albano de Oliveira',
+    img: null
+  },
+  {
+    name: 'Lucas Xavier Ebling Pereira',
+    img: 'xavier.jpg'
+  },
+  {
+    name: 'Lui Franco Rocha',
+    img: 'lui.jpg'
+  },
+  {
+    name: 'Luís Eduardo Rozante de Freitas Pereira',
+    img: 'luis.jpg'
+  },
+  {
+    name: 'Luís Felipe Ribeiro Chaves',
+    img: 'lchaves.jpg'
+  },
+  {
+    name: 'Marcelo Temoteo de Castro',
+    img: 'temoteo.jpg'
+  },
+  {
+    name: 'Matheus Bragança de Souza',
+    img: 'mash.jpg'
+  },
+  {
+    name: 'Michel Palmezan Ribeiro De Grande',
+    img: 'michel.jpg'
+  },
+  {
+    name: 'Michelle Wingter da Silva',
+    img: 'michelle.jpg'
+  },
+  {
+    name: 'Miguel de Mattos Gardini',
+    img: 'miguel.jpg'
+  },
+  {
+    name: 'Óliver Savastano Becker',
+    img: 'oliver.jpg'
+  },
+  {
+    name: 'Priscila Ribeiro Zucato',
+    img: 'pri.jpg'
+  },
+  {
+    name: 'Rafael Miranda Lopes',
+    img: 'miranda.jpg'
+  },
+  {
+    name: 'Rafael Monteiro',
+    img: 'joegs.jpg'
+  },
+  {
+    name: 'Rafael Pedrosa Silva Clerici',
+    img: 'lag.jpg'
+  },
+  {
+    name: 'Raul Wagner Martins Costa',
+    img: 'raul.jpg'
+  },
+  {
+    name: 'Renata Vinhaga dos Anjos',
+    img: 'reva.jpg'
+  },
+  {
+    name: 'Ricardo Luiz Cardoso Menezes',
+    img: 'pequi.jpg'
+  },
+  {
+    name: 'Rodrigo Geurgas Zavarizz',
+    img: 'rodrigo.jpg'
+  },
+  {
+    name: 'Thiago Carvalho Gonçalves',
+    img: 'tgoncalves.jpg'
+  },
+  {
+    name: 'Thiago Daniel Cagnoni de Pauli',
+    img: 'float.jpg'
+  },
+  {
+    name: 'Tiago Marino Silva',
+    img: 'tmarino.jpg'
+  },
+  {
+    name: 'Tiago Silva Miranda',
+    img: 'tmiranda.jpg'
+  },
+  {
+    name: 'Vinicius Ricardo Carvalho',
+    img: 'vinicius.jpg'
+  },
+  {
+    name: 'Vitor Santana Cordeiro',
+    img: 'vitor.jpg'
+  },
+  {
+    name: 'William Paulo Gonçalves Braz',
+    img: 'will.jpg'
+  },
+  {
+    name: 'Willian Gonzaga Leodegario',
+    img: 'binario.jpg'
+  },
+  {
+    name: 'Yago Pessoa',
+    img: 'yago.jpg'
+  }
+]
+
+export default members
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,3 +1,81 @@
 <template>
-  <h1>Quem somos</h1>
+  <v-layout class="page">
+    <v-flex xs12 sm10 offset-sm1>
+      <p class="display-1 text-xs-center mt-4 pt-3">Conheça nossos membros:</p>
+        <v-container grid-list-lg fluid>
+          <v-layout row wrap>
+            <v-flex v-for="item in itens" :key="item.name">
+                <v-card tile>
+                    <v-img
+                        v-if="item.img"
+                        :src="'/static/img/members/'+item.img"
+                        height="300px"
+                        class="img"
+                        alt="Foto do membro"
+                    >
+                      <v-layout
+                        slot="placeholder"
+                        fill-height
+                        align-center
+                        justify-center
+                        ma-0
+                      >
+                        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                      </v-layout>
+                    </v-img>
+                    <v-img
+                        v-else
+                        src="/static/img/members/default.png"
+                        height="300px"
+                        class="img"
+                        alt="Imagem padrão para membro sem foto"
+                    >
+                      <v-layout
+                        slot="placeholder"
+                        fill-height
+                        align-center
+                        justify-center
+                        ma-0
+                      >
+                        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+                      </v-layout>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <h2 class="headline mb-0">{{item.name}}</h2>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-container fluid text-xs-center>
+          <p class="caption mt-2">
+            Site desenvolvido por <a href="https://www.linkedin.com/in/yagopessoa/">Yago Pessoa</a>. Copyright © 2019 Fellowship of The Game. Ícones por <a href="https://www.flaticon.com">FlatIcon</a>.
+          </p>
+        </v-container>
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+
+import members from '@/members.js'
+
+export default {
+  data () {
+    return {
+      itens: members
+    }
+  },
+  name: 'AboutPage'
+}
+</script>
+
+<style>
+  .page{
+    background-color: #ECEFF1;
+  }
+  .img{
+    min-width: 288px;
+  }
+</style>
