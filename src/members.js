dd61a5068dfa1f68807e9cbc5a1ee8a18eedb2a5
Yago Pessoa <yagorp15@gmail.com>
2019-02-24 12:42:17 -0300

    Showing area icons at members list

--- src/members.js
+++ src/members.js
@@ -1,251 +1,932 @@
 const members = [
   {
     name: 'Abner Eduardo Silveira Santos',
-    img: 'abner.jpg'
+    img: 'abner.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Amira Gayatri El Assad Hempel',
-    img: 'amira.jpg'
+    img: 'amira.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Augusto Paulo Silva',
-    img: 'augusto.jpg'
+    img: 'augusto.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Bruno Baldissera Carlotto',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Bruno Gazoni',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Daniel Fróes',
-    img: 'froes.jpg'
+    img: 'froes.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Daniel Sá Barretto Prado Garcia',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'David Souza Rodrigues',
-    img: 'david.jpg'
+    img: 'david.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Edson Yudi Toma',
-    img: 'edson.jpg'
+    img: 'edson.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Eike João Sanches Sato',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Eleazar Fernando Braga',
-    img: 'eleazar.jpg'
+    img: 'eleazar.jpg',
+    areas: {
+      coord: true,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: true,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Elisa Saltori Trujillo',
-    img: 'elisa.jpg'
+    img: 'elisa.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Enzo Paulo Sanches Sato',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Fabiana Dalacqua Mendes',
-    img: 'fabiana.jpg'
+    img: 'fabiana.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Fabrício Guedes Faria',
-    img: 'bibicio.jpg'
+    img: 'bibicio.jpg',
+    areas: {
+      coord: true,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: true,
+      dev: true,
+      sound: true,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Felipe Contini Torres',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Fernando Gorgulho Fayet',
-    img: 'fer.jpg'
+    img: 'fer.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: true,
+      dev: true,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Gabriel Arantes de Lima e Silva',
-    img: 'lombardi.jpg'
+    img: 'lombardi.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Gabriel Mattheus Bezerra Alves de Carvalho',
-    img: 'gabriel.jpg'
+    img: 'gabriel.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Gabriel Romualdo Silveira Pupo',
-    img: 'pupo.jpg'
+    img: 'pupo.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: true,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Gabriel Simmel Nascimento',
-    img: 'cartaz.jpg'
+    img: 'cartaz.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Giovanni Paolo Meloni',
-    img: 'giovanni.jpg'
+    img: 'giovanni.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Giuliano Lourençon',
-    img: 'psy.jpg'
+    img: 'psy.jpg',
+    areas: {
+      coord: true,
+      rp: true,
+      rh: false,
+      lead: true,
+      design: true,
+      dev: true,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Guilherme López Barros',
-    img: null
+    img: null,
+    areas: {
+      coord: true,
+      rp: true,
+      rh: true,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Gustavo de Moura Souza',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: true,
+      design: false,
+      dev: false,
+      sound: false,
+      art: true,
+      research: true
+    }
   },
   {
     name: 'Gustavo Sasaki Roncaglia',
-    img: 'sasaki.jpg'
+    img: 'sasaki.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Gyovana Mayara Moriyama',
-    img: 'gyo.jpg'
+    img: 'gyo.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: true
+    }
   },
   {
     name: 'Henrique Matarazo Camillo',
-    img: 'nintendo.jpg'
+    img: 'nintendo.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: true,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Jean Sidharta Rocha Guzman',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Jorge Luiz Bulhões Tosta',
-    img: 'jorge.jpg'
+    img: 'jorge.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Leonardo Chieppe',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: true,
+      dev: true,
+      sound: true,
+      art: true,
+      research: true
+    }
   },
   {
     name: 'Leonardo Maronezi Pizani',
-    img: 'maronezi.jpg'
+    img: 'maronezi.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Leonardo Tórtoro Pereira',
-    img: 'leo.jpg'
+    img: 'leo.jpg',
+    areas: {
+      coord: true,
+      rp: true,
+      rh: false,
+      lead: true,
+      design: true,
+      dev: true,
+      sound: false,
+      art: true,
+      research: true
+    }
   },
   {
     name: 'Livia Scienza',
-    img: null
+    img: null,
+    areas: {
+      coord: true,
+      rp: false,
+      rh: true,
+      lead: false,
+      design: true,
+      dev: false,
+      sound: false,
+      art: false,
+      research: true
+    }
   },
   {
     name: 'Lucas Albano de Oliveira',
-    img: null
+    img: null,
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Lucas Xavier Ebling Pereira',
-    img: 'xavier.jpg'
+    img: 'xavier.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Lui Franco Rocha',
-    img: 'lui.jpg'
+    img: 'lui.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: true,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Luís Eduardo Rozante de Freitas Pereira',
-    img: 'luis.jpg'
+    img: 'luis.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Luís Felipe Ribeiro Chaves',
-    img: 'lchaves.jpg'
+    img: 'lchaves.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: true,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Marcelo Temoteo de Castro',
-    img: 'temoteo.jpg'
+    img: 'temoteo.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Matheus Bragança de Souza',
-    img: 'mash.jpg'
+    img: 'mash.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Michel Palmezan Ribeiro De Grande',
-    img: 'michel.jpg'
+    img: 'michel.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Michelle Wingter da Silva',
-    img: 'michelle.jpg'
+    img: 'michelle.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Miguel de Mattos Gardini',
-    img: 'miguel.jpg'
+    img: 'miguel.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: true,
+      dev: false,
+      sound: true,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Óliver Savastano Becker',
-    img: 'oliver.jpg'
+    img: 'oliver.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Priscila Ribeiro Zucato',
-    img: 'pri.jpg'
+    img: 'pri.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Rafael Miranda Lopes',
-    img: 'miranda.jpg'
+    img: 'miranda.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: true,
+      design: true,
+      dev: true,
+      sound: false,
+      art: false,
+      research: true
+    }
   },
   {
     name: 'Rafael Monteiro',
-    img: 'joegs.jpg'
+    img: 'joegs.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Rafael Pedrosa Silva Clerici',
-    img: 'lag.jpg'
+    img: 'lag.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Raul Wagner Martins Costa',
-    img: 'raul.jpg'
+    img: 'raul.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Renata Vinhaga dos Anjos',
-    img: 'reva.jpg'
+    img: 'reva.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Ricardo Luiz Cardoso Menezes',
-    img: 'pequi.jpg'
+    img: 'pequi.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Rodrigo Geurgas Zavarizz',
-    img: 'rodrigo.jpg'
+    img: 'rodrigo.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Thiago Carvalho Gonçalves',
-    img: 'tgoncalves.jpg'
+    img: 'tgoncalves.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: true,
+      lead: false,
+      design: true,
+      dev: false,
+      sound: false,
+      art: true,
+      research: false
+    }
   },
   {
     name: 'Thiago Daniel Cagnoni de Pauli',
-    img: 'float.jpg'
+    img: 'float.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: true,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Tiago Marino Silva',
-    img: 'tmarino.jpg'
+    img: 'tmarino.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Tiago Silva Miranda',
-    img: 'tmiranda.jpg'
+    img: 'tmiranda.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Vinicius Ricardo Carvalho',
-    img: 'vinicius.jpg'
+    img: 'vinicius.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: true,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Vitor Santana Cordeiro',
-    img: 'vitor.jpg'
+    img: 'vitor.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'William Paulo Gonçalves Braz',
-    img: 'will.jpg'
+    img: 'will.jpg',
+    areas: {
+      coord: false,
+      rp: false,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Willian Gonzaga Leodegario',
-    img: 'binario.jpg'
+    img: 'binario.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   },
   {
     name: 'Yago Pessoa',
-    img: 'yago.jpg'
+    img: 'yago.jpg',
+    areas: {
+      coord: false,
+      rp: true,
+      rh: false,
+      lead: false,
+      design: false,
+      dev: false,
+      sound: false,
+      art: false,
+      research: false
+    }
   }
 ]
 
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -4,7 +4,7 @@
       <p class="display-1 text-xs-center mt-4 pt-3">Conheça nossos membros:</p>
         <v-container grid-list-lg fluid>
           <v-layout row wrap>
-            <v-flex v-for="item in itens" :key="item.name">
+            <v-flex v-for="item in itens" :key="item.name" grow>
                 <v-card tile>
                     <v-img
                         v-if="item.img"
@@ -42,7 +42,57 @@
                     </v-img>
 
                     <v-card-title primary-title>
+                      <div>
                         <h2 class="headline mb-0">{{item.name}}</h2>
+                        <div class="areas-line my-2 pl-1">
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.coord" src="/static/img/areas/box-lead.png" alt="Ícone da Coordenação" class="area-icon" slot="activator" />
+                            <span>Coordenação</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.rh" src="/static/img/areas/box-rh.png" alt="Ícone de Relações Humanas" class="area-icon" slot="activator" />
+                            <span>RH</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.rp" src="/static/img/areas/box-rp.png" alt="Ícone de Relações Públicas" class="area-icon" slot="activator" />
+                            <span>RP</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.lead" src="/static/img/areas/box-rh.png" alt="Ícone de Gestão" class="area-icon" slot="activator" />
+                            <span>Gestão</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.design" src="/static/img/areas/box-design.png" alt="Ícone de Game Design" class="area-icon" slot="activator" />
+                            <span>Game Design</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.dev" src="/static/img/areas/box-code.png" alt="Ícone de Programador" class="area-icon" slot="activator" />
+                            <span>Dev</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.sound" src="/static/img/areas/box-sound.png" alt="Ícone de Sons" class="area-icon" slot="activator" />
+                            <span>Som</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.art" src="/static/img/areas/box-art.png" alt="Ícone de Arte" class="area-icon" slot="activator" />
+                            <span>Arte</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img v-if="item.areas.research" src="/static/img/areas/box-research.png" alt="Ícone de Pesquisa" class="area-icon" slot="activator" />
+                            <span>Pesquisa</span>
+                          </v-tooltip>
+                          <v-tooltip bottom>
+                            <img 
+                              v-if="!item.areas.research && !item.areas.coord && !item.areas.rh && !item.areas.rp && !item.areas.lead && !item.areas.design && !item.areas.dev && !item.areas.sound && !item.areas.art"
+                              src="/static/img/areas/box-none.png"
+                              alt="Ícone de nenhuma área"
+                              class="area-icon"
+                              slot="activator"
+                            />
+                            <span>Indefinido</span>
+                          </v-tooltip>
+                        </div>
+                      </div>
                     </v-card-title>
                 </v-card>
             </v-flex>
@@ -77,5 +127,21 @@ export default {
   }
   .img{
     min-width: 288px;
+    min-height: 288px;
+  }
+  .areas-line{
+    display: flex;
+    align-items: center;
+  }
+  .area-icon{
+    width: 32px;
+    height: 32px;
+    margin-right: 8px;
+  }
+
+  @media screen and (max-width: 359px) {
+    .area-icon{
+      margin-right: 4px;
+    }
   }
 </style>
